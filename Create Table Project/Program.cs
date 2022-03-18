1b93a77976cf4c2b4a3aa8c830364395c4d52f53
nourinalam <nourinalam26@gmail.com>
2022-03-18 11:17:19 +0700

    Menulis Code sesuai pdf

--- Create Table Project/Program.cs
+++ Create Table Project/Program.cs
@@ -11,7 +11,35 @@ namespace Create_Table_Project
     {
         static void Main(string[] args)
         {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source = LAPTOP-8JHE7APE\\ALAM;database = ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah." + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+
 
         }
+
     }
 }
